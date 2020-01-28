8df67f7e2e3760370ab021e9da8ed10c1657f59a
Your Name <you@example.com>
2020-01-28 21:37:45 +0300

    Поправил стиль кода Александра

---
+++ Alex03/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    uint8_t n = 0;
+    cin >> (int&)n;
+    uint8_t h[n];
+    uint8_t identical = n/5;
+
+    uint8_t min = 0;
+    uint8_t max = 0;
+    for (uint8_t i = 0; i < n; ++i) {
+
+        cin >> (int&)h[i];
+
+        if (h[i] < h[min]) {
+            min = i;
+        }
+
+        if (h[i] > h[max]) {
+            max = i;
+        }
+    }
+
+    uint8_t star = 0;
+    uint8_t k = 0;
+    for (uint8_t i = 0; i < n; ++i) {
+        if (k == 0) {
+            star++;
+        }
+
+        if (k == identical - 1) {
+            k = 0;
+        }
+        else {
+            k++;
+        }
+
+        h[min] = star;
+        min = max;
+
+        for (uint8_t i = 0; i < n; ++i) {
+            if (h[i] < h[min] && h[i] > star) {
+                min = i;
+            }
+        }
+    }
+
+    for (uint8_t i = 0; i < n; ++i) {
+        cout << static_cast<uint16_t>(h[i]) << ' ';
+    }
+
+    return 0;
+}
